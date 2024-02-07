50f00d20dc87446674fd043fffc2933cf0e73a2a
William Herr <herrwilliam@outlook.com>
2024-02-07 00:03:08 -0500

    remove fillers and added assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,5 @@
 import logo from "./logo.svg";
 import backend from "./backend.png";
-import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
@@ -8,7 +7,6 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
@@ -21,26 +19,21 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import abacus from "./company/abacus_group_logo.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import rush from "./project/rush.jpg";
+import tilt from "./project/tilt.jpg";
+import flexibble from "./project/flexibble.png";
 
 export {
   logo,
   backend,
-  creator,
   mobile,
   web,
   github,
   menu,
   close,
   css,
-  docker,
   figma,
   git,
   html,
@@ -52,11 +45,8 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+  abacus,
+  rush,
+  tilt,
+  flexibble,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,265 +1,227 @@
 import {
-    mobile,
-    backend,
-    creator,
-    web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
-    nodejs,
-    mongodb,
-    git,
-    figma,
-    docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
-    carrent,
-    jobit,
-    tripguide,
-    threejs,
+  mobile,
+  backend,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  threejs,
+  abacus,
+  rush,
+  tilt,
+  flexibble,
 } from "../assets";
 
 export const navLinks = [
-    {
-        id: "about",
-        title: "About",
-    },
-    {
-        id: "work",
-        title: "Work",
-    },
-    {
-        id: "contact",
-        title: "Contact",
-    },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
 ];
 
 const services = [
-    {
-        title: "Web Developer",
-        icon: web,
-    },
-    {
-        title: "React Native Developer",
-        icon: mobile,
-    },
-    {
-        title: "Backend Developer",
-        icon: backend,
-    },
-    {
-        title: "Content Creator",
-        icon: creator,
-    },
+  {
+    title: "Software Engineer",
+    icon: web,
+  },
+  {
+    title: "Full Stack",
+    icon: mobile,
+  },
+  {
+    title: "Next",
+    icon: backend,
+  },
+  {
+    title: "React",
+    icon: backend,
+  },
 ];
 
 const technologies = [
-    {
-        name: "HTML 5",
-        icon: html,
-    },
-    {
-        name: "CSS 3",
-        icon: css,
-    },
-    {
-        name: "JavaScript",
-        icon: javascript,
-    },
-    {
-        name: "TypeScript",
-        icon: typescript,
-    },
-    {
-        name: "React JS",
-        icon: reactjs,
-    },
-    {
-        name: "Redux Toolkit",
-        icon: redux,
-    },
-    {
-        name: "Tailwind CSS",
-        icon: tailwind,
-    },
-    {
-        name: "Node JS",
-        icon: nodejs,
-    },
-    {
-        name: "MongoDB",
-        icon: mongodb,
-    },
-    {
-        name: "Three JS",
-        icon: threejs,
-    },
-    {
-        name: "git",
-        icon: git,
-    },
-    {
-        name: "figma",
-        icon: figma,
-    },
-    {
-        name: "docker",
-        icon: docker,
-    },
+  {
+    name: "HTML",
+    icon: html,
+  },
+  {
+    name: "CSS",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
 ];
 
 const experiences = [
-    {
-        title: "React.js Developer",
-        company_name: "Starbucks",
-        icon: starbucks,
-        iconBg: "#383E56",
-        date: "March 2020 - April 2021",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "React Native Developer",
-        company_name: "Tesla",
-        icon: tesla,
-        iconBg: "#E6DEDD",
-        date: "Jan 2021 - Feb 2022",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Web Developer",
-        company_name: "Shopify",
-        icon: shopify,
-        iconBg: "#383E56",
-        date: "Jan 2022 - Jan 2023",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Full stack Developer",
-        company_name: "Meta",
-        icon: meta,
-        iconBg: "#E6DEDD",
-        date: "Jan 2023 - Present",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
+  {
+    title: "Full Stack Software Engineer",
+    company_name: "Abacus Group LLC",
+    icon: abacus,
+    iconBg: "#FFFFFF",
+    date: "August 2019",
+    points: [
+      "CRM: Maintained dashboards and created web applications.",
+      "Developed MVC web pages using C#, JavaScript, HTML, and CSS.",
+      "Assisted in integrating a new portal, which involved creating web pages with React/Vite, configuring IIS rewrites, and setting up YAML file pipelines for Azure DevOps CI/CD.",
+      "Implemented efficient algorithms and data structures to optimize software performance, resulting in a 99% faster page loading speed.",
+      "Utilized Azure DevOps and Bitbucket for source control management.",
+      "Created SQL scripts to query complex data and generate reports.",
+      "Established SFTP connections with clients and vendors for secure file transfers.",
+      "Assisted in maintaining authorization by integrating with OKTA.",
+      "Developed REST and SOAP APIs using .NET Core 7 and Swagger",
+    ],
+  },
 ];
 
+const tagColors = {
+  code: "blue-text-gradient",
+  styles: "pink-text-gradient",
+  db: "green-text-gradient",
+  other: "orange-text-gradient",
+};
+
 const testimonials = [
-    {
-        testimonial:
-            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-        name: "Sara Lee",
-        designation: "CFO",
-        company: "Acme Co",
-        image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-        testimonial:
-            "I've never met a web developer who truly cares about their clients' success like Rick does.",
-        name: "Chris Brown",
-        designation: "COO",
-        company: "DEF Corp",
-        image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-        testimonial:
-            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-        name: "Lisa Wang",
-        designation: "CTO",
-        company: "456 Enterprises",
-        image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
+  {
+    testimonial:
+      "Will was a rockstar and I could always rely on him to get things done",
+    name: "Zeng Xiong",
+    designation: "Senior Software Engineer",
+    company: "Abacus Group LLC",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
 ];
 
 const projects = [
-    {
-        name: "Car Rent",
-        description:
-            "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-        tags: [
-            {
-                name: "react",
-                color: "blue-text-gradient",
-            },
-            {
-                name: "mongodb",
-                color: "green-text-gradient",
-            },
-            {
-                name: "tailwind",
-                color: "pink-text-gradient",
-            },
-        ],
-        image: carrent,
-        source_code_link: "https://github.com/",
-    },
-    {
-        name: "Job IT",
-        description:
-            "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-        tags: [
-            {
-                name: "react",
-                color: "blue-text-gradient",
-            },
-            {
-                name: "restapi",
-                color: "green-text-gradient",
-            },
-            {
-                name: "scss",
-                color: "pink-text-gradient",
-            },
-        ],
-        image: jobit,
-        source_code_link: "https://github.com/",
-    },
-    {
-        name: "Trip Guide",
-        description:
-            "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-        tags: [
-            {
-                name: "nextjs",
-                color: "blue-text-gradient",
-            },
-            {
-                name: "supabase",
-                color: "green-text-gradient",
-            },
-            {
-                name: "css",
-                color: "pink-text-gradient",
-            },
-        ],
-        image: tripguide,
-        source_code_link: "https://github.com/",
-    },
+  {
+    name: "Rush",
+    description:
+      "Often, when students emailed professors, they took several days to respond to a simple question. Rush is a direct messaging app for students and professors, designed as an alternative to email. Professors can create classes and students can create groups. Users can message each other and the messages are updated in real time.",
+    tags: [
+      {
+        name: "android studio",
+        color: tagColors.code,
+      },
+      {
+        name: "tailwind",
+        color: tagColors.styles,
+      },
+      {
+        name: "firebase",
+        color: tagColors.db,
+      },
+    ],
+    image: rush,
+    source_code_link: "https://github.com/Williamherr/rush",
+  },
+  {
+    name: "Tilt",
+    description: `Tilt is a Thread clone web application. It provides a user-friendly platform for seamless and
+                  interactive communication. Users can post and read both personal and community
+                  threads, fostering a vibrant and engaging online community.
+                  `,
+    tags: [
+      {
+        name: "Next.js",
+        color: tagColors.code,
+      },
+      {
+        name: "mongodbb",
+        color: tagColors.db,
+      },
+      {
+        name: "shadcn-ui",
+        color: tagColors.styles,
+      },
+
+      {
+        name: "Clerk Auth",
+        color: tagColors.other,
+      },
+      {
+        name: "uploadthing",
+        color: tagColors.other,
+      },
+    ],
+    image: tilt,
+    source_code_link: "https://github.com/Williamherr/tilt",
+  },
+  {
+    name: "Flexibble",
+    description:
+      "Flexibble is a Dribbble-inspired web application, offering a platform for designers to share their work. It’s built with modern technologies for a seamless user experience and efficient data management, ensuring a secure and innovative platform for showcasing design work.",
+    tags: [
+      {
+        name: "next.js",
+        color: tagColors.code,
+      },
+      {
+        name: "graphql",
+        color: tagColors.db,
+      },
+      {
+        name: "tailwind",
+        color: tagColors.styles,
+      },
+      {
+        name: "cloudinary",
+        color: tagColors.other,
+      },
+      {
+        name: "next auth",
+        color: tagColors.other,
+      },
+    ],
+    image: flexibble,
+    source_code_link: "https://github.com/Williamherr/grafbase_flexibble",
+  },
 ];
 
 export { services, technologies, experiences, testimonials, projects };
