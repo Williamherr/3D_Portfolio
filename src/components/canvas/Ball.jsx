f43960e150dd718a619c7663c481e55cb3507f1e
William Herr <herrwilliam@outlook.com>
2024-02-07 00:05:03 -0500

    Added decor to both side of the ball and increase rotation speed

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -31,6 +31,12 @@ const Ball = (props) => {
           flatShading
           map={decal}
         />
+        <Decal
+          position={[0, 0, -1]} // position it on the other side
+          rotation={[2 * Math.PI, Math.PI, 6.25]} // rotate it around the y-axis by π
+          flatShading
+          map={decal}
+        />
       </mesh>
     </Float>
   );
@@ -46,7 +52,7 @@ const BallCanvas = ({ icon }) => {
           // minPolarAngle={Math.PI / 6}
           // maxPolarAngle={Math.PI - Math.PI / 6}
           autoRotate
-          autoRotateSpeed={0.75}
+          autoRotateSpeed={1}
           enableZoom={false}
         />
         <Ball imgUrl={icon} />
